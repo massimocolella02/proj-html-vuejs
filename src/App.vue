07749ab9d07605d30a8d787bae2af9add50ba431
Massimo Colella <massimo-colella@outlook.it>
2023-05-15 09:52:58 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script>
+import NavBar from './components/NavBar.vue';
+export default{
+  name: 'App',
+  components:{
+    NavBar
+  }
+}
+</script>
+
+<template>
+  <NavBar/>
+</template>
+
+<style>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,73 @@
+<template>
+    <nav>
+        <div class="container">
+            <div class="l-side">
+                <img src="/images/logotype.png" alt="logo">
+            </div>
+            <div class="r-side">
+                <ul>
+                    <li>
+                        <a href="">home</a>
+                    </li>
+                    <li>
+                        <a href="">about</a>
+                    </li>
+                    <li>
+                        <a href="">projects</a>
+                    </li>
+                    <li>
+                        <a href="">services</a>
+                    </li>
+                    <li>
+                        <a href="">blog</a>
+                    </li>
+                    <li>
+                        <a href="">contact</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: 'NavBar'
+}
+</script>
+
+<style scoped>
+nav{
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+}
+nav .container{
+    width: 90%;
+    height: 60px;
+    margin: 0 auto;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+nav ul{
+    display: flex;
+}
+nav ul li{
+    margin-right: 15px;
+    line-height: 60px;
+}
+nav ul li a{
+    text-transform: uppercase;
+}
+nav ul li a:hover{
+    color: white;
+}
+nav ul li:hover{
+    border-top: 2px solid white;
+}
+.l-side img{
+    height: 20px;
+}
+</style>
